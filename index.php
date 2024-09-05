5fddcf6f6018344623cef4300cbdb50b4f133a6b
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-09-05 16:47:58 +0300

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($argc != 3) {
+    fwrite(STDERR, "Кол-во аргументов должно быть два!" . PHP_EOL);
+} else if (!is_numeric($argv[1])) {
+    fwrite(STDERR, "Первый аргумент не число!" . PHP_EOL);
+} else if (!is_numeric($argv[2])) {
+    fwrite(STDERR, "Второй аргумент не число!" . PHP_EOL);
+} else if ($argv[2] == 0) {
+    fwrite(STDERR, "Второй аргумент равен 0. На 0 делить нельзя!" . PHP_EOL);
+} else {
+    fwrite(STDERR, "$argv[1] / $argv[2] = " . $argv[1] / $argv[2] . PHP_EOL);
+}
